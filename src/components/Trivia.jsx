1e1d54fd8d99dd0607ae2abd15c36e7424730a56
Arik Alexandrov <arikxl@gmail.com>
2021-09-03 16:03:51 +0300

    main

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const trivia = () => {
+    return (
+        <div className="trivia">
+            <div className="question">lama?</div>
+            <div className="answers">
+                <div className="answer">1</div>
+                <div className="answer">2</div>
+                <div className="answer">3</div>
+                <div className="answer">4</div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default trivia
